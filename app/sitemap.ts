5663fa4947454e73ad69f20f14817aa079f933b0
Raj Gupta <rajgupta.ckt22@gmail.com>
2026-02-01 15:13:13 +0530

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,5 +1,8 @@
 import { MetadataRoute } from "next";
 import { generateCourseSlug } from "@/components/slugify";
+import { ArticleSchema } from "@/components/article/schema";
+
+const baseUrl = "https://skillhub-student.vercel.app";
 
 async function courseURL() {
   const baseUrl = "https://skillhub-student.vercel.app";
@@ -14,6 +17,20 @@ async function courseURL() {
     }))
   );
   return courseLinks;
+}
+
+async function articleURL() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/article`);
+    if (!res.ok) return [];
+    const data = await res.json();
+    const articles=data.articles;
+    const articleLinks=articles.map((a:ArticleSchema)=>{
+      return {
+        url: `${baseUrl}/${a.type}/${a.slug}`,
+        lastModified: new Date(a.updatedAt),
+      }
+    })
+    return articleLinks;
 }
 
 async function materialLinks(){
@@ -36,6 +53,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap>{
   const baseUrl = "https://skillhub-student.vercel.app";
   const courseLinks = await courseURL();
   const materialLink=await materialLinks();
+  const articleLinks=await articleURL();
   return [
     { url: baseUrl, lastModified: new Date(), changeFrequency: "weekly", priority: 1 },
 
@@ -45,10 +63,18 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap>{
     //Material links
     ...materialLink,
 
+    //Articles Blog Tutorial links
+    ...articleLinks,
+
     // Public pages
     { url: `${baseUrl}/about`, lastModified: new Date(), priority: 0.9 },
+    { url: `${baseUrl}/learn`, lastModified: new Date(), priority: 0.9 },
+    { url: `${baseUrl}/materials`, lastModified: new Date(), priority: 0.9 },
+    { url: `${baseUrl}/course`, lastModified: new Date(), priority: 0.9 },
+
     { url: `${baseUrl}/contact`, lastModified: new Date(), priority: 0.8 },
     { url: `${baseUrl}/faq`, lastModified: new Date(), priority: 0.8 },
+
 
     // Auth pages
     { url: `${baseUrl}/auth/`, lastModified: new Date(), priority: 0.6 },
