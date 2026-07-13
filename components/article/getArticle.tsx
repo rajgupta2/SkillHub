e682c4eb6b158f695fd4b835c9fff91bd6706a27
Raj Gupta <rajgupta.ckt22@gmail.com>
2026-07-13 10:24:04 +0530

    Solved wrong url

--- components/article/getArticle.tsx
+++ components/article/getArticle.tsx
@@ -1,5 +1,5 @@
-export async function getArticleBySlug(slug:string) {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/article/slug/${slug}`);
-    const data = await res.json();
-    return data.article;
+export async function getArticleBySlug(slug: string) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/article/${slug}`);
+  const data = await res.json();
+  return data.article;
 }
